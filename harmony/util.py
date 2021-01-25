785c206ff9aa5004e7cf3827d31fa16371e569b2
Kevin Beam <kbeam@nsidc.org>
2021-01-24 22:28:50 -0700

    Clean up notebook & example test

---
+++ harmony/util.py
@@ -0,0 +1,26 @@
+import requests
+
+ENVIRONMENT_HOSTNAME = {
+    'sbx': 'harmony.sbx.earthdata.nasa.gov',
+    'sit': 'harmony.sit.earthdata.nasa.gov',
+    'uat': 'harmony.uat.earthdata.nasa.gov',
+    'prod': 'harmony.earthdata.nasa.gov'
+}
+
+
+def _harmony_hostname(env: str) -> str:
+    return ENVIRONMENT_HOSTNAME[env]
+
+
+def _url(environment: str, uri: str) -> str:
+    return f'https://{_harmony_hostname(environment)}/{uri}'
+
+
+def find_by_attr(items: list, attr: str, val: str) -> str:
+    for i in items:
+        if i[attr] == val:
+            return i
+
+
+def api(environment: str, uri: str):
+    return requests.get(_url(environment, uri)).json()
