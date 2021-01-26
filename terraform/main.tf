ebaa89f30de1bd2f630ed28390f0b8257af7d7b9
James Norton <james@element84.com>
2021-01-26 12:52:50 -0500

    Add user data for EC2 instance to install Docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "harmony_regression_test" {
-  name        = "harmony-sg-${var.environment_name}"
+  name        = "harmony-regression-test-sg-${var.environment_name}"
   description = "Allow SSH and harmony endpoint traffic for ${var.environment_name}"
   vpc_id      = data.aws_vpc.app.id
 
@@ -40,25 +40,14 @@ resource "aws_instance" "harmony_regression_test" {
   ami                  = data.aws_ami.ngap.id
   subnet_id            = tolist(data.aws_subnet_ids.app.ids)[0]
   instance_type        = var.instance_type
-  iam_instance_profile = data.aws_iam_instance_profile.regression_test_instance_profile
+  iam_instance_profile = data.aws_iam_instance_profile.regression_test_instance_profile.name
   key_name             = var.key_name
-  count                = 1
 
-  # user_data = templatefile("${path.module}/harmony-user-data.tmpl", {
-  #   environment_name = var.environment_name
-  #   ssh_keys_bucket  = aws_s3_bucket.authorized_keys.id
-  #   }
-  # )
+  user_data = file("${path.module}/harmony-user-data.tmpl")
 
   vpc_security_group_ids = [aws_security_group.harmony_regression_test.id]
   tags = {
     Name = "harmony-regression-test-${var.environment_name}"
-  }
-
-  provisioner "file" {
-    # TODO move main directory contents to subdir to avoid copying terraform files
-    source      = ".."
-    destination = "/home/ec2-user"
   }
 
   lifecycle {
