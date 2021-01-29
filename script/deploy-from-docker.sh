1e45d4b340a4c5b6b53d1b119d5cd48dbc9b707d
Chris Durbin <christopher.d.durbin@nasa.gov>
2021-01-29 09:34:11 -0500

    HARMONY-570: Needed to change to deploy from docker in order to be able to use SSM in Bamboo.

---
+++ script/deploy-from-docker.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -ex
+
+if [ -e .deployenv ]; then
+  set -o allexport
+  source .deployenv
+  set +o allexport
+fi
+
+function retry {
+  set +e
+  local retries=$1
+  shift
+
+  local count=0
+  until "$@"; do
+    exit=$?
+    count=$(($count + 1))
+    if [ $count -lt $retries ]; then
+      sleep 10
+      echo "Retry $count/$retries exited $exit, retrying"
+    else
+      echo "Retry $count/$retries exited $exit, no more retries left."
+      set -e
+      return $exit
+    fi
+  done
+  set -e
+  return 0
+}
+
+retry 5 scp -v -F sshconfig -i .identity -r test "ec2-user@${INSTANCE_ID}:"
+retry 5 ssh -F sshconfig -i .identity "ec2-user@${INSTANCE_ID}" "cd test && make image"
+set +e
+ssh -F sshconfig -i .identity "ec2-user@${INSTANCE_ID}" "cd test && make run HARMONY_HOST_URL=${HARMONY_HOST_URL}"
+exit_code=$?
+set -e
+# copy the output to here
+retry 5 scp -F sshconfig -i .identity "ec2-user@${INSTANCE_ID}:test/output/*.ipynb" ./output
+
+# return the exit code form running the tests
+exit $exit_code
