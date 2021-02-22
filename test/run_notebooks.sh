2093e429dbaf8cd293b4195a1fbe54c8d1e6bbf5
James Norton <james@element84.com>
2021-02-22 17:06:57 -0500

    HARMONY-571: Use mount point for notebook files to make development quicker

--- test/run_notebooks.sh
+++ test/run_notebooks.sh
@@ -6,11 +6,11 @@ NC='\033[0m' # No Color
 echo "Running regression tests"
 
 # Add Docker images created in Makefile here
-images=(harmony asf-gdal harmony-regression)
+images=(harmony-regression)
 
 # launch all the docker containers and store their process IDs
 for image in ${images[@]}; do
-  PIDS+=(${image},$(docker run -d -v ${PWD}/output:/root/output --env harmony_host_url="${HARMONY_HOST_URL}" "harmony/regression-tests-${image}:latest"))
+  PIDS+=(${image},$(docker run -d -v ${PWD}/output:/root/output -v ${PWD}/${image}:/root/${image} --env harmony_host_url="${HARMONY_HOST_URL}" "harmony/regression-tests-${image}:latest"))
 done
 
 # wait for processes to finish and store each exit code into array STATUS'
