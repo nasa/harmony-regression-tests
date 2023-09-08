2eaa6c2166c46a30be2fadaad63e6e182e240c66
Josie Lyon <josie.lyon@nasa.gov>
2023-09-07 20:53:42 -0400

    DAS-1867: Updated utilties.py function to use DataTrees instead of Datasets.

--- test/trajectory-subsetter/utilities.py
+++ test/trajectory-subsetter/utilities.py
@@ -7,28 +7,26 @@ from os import listdir, remove, replace
 
 from harmony import Client, Request
 from harmony.harmony import ProcessingFailedException
-import xarray as xr
+import datatree
 
 
 def compare_results_to_reference_file(results_file_name: str,
-                                      reference_file_name: str,
-                                      group_path: str) -> None:
-    """ Use native `xarray` functionality to compare data values and metadata
-        attributes of downloaded results to a reference file.
+                                      reference_file_name: str) -> None:
+    """ Use `DataTree` functionality to compare data values, variables, 
+        coordinates, metadata, and all their corresponding attributes of
+        downloaded results to a reference file.
 
     """
-    reference_data =  xr.open_dataset(reference_file_name, group=group_path)
-    results_data = xr.open_dataset(results_file_name, group=group_path)
+    reference_data =  datatree.open_datatree(reference_file_name)
+    results_data = datatree.open_datatree(results_file_name)
 
-    assert results_data.equals(reference_data), ('Output and reference files '
-                                                 'do not match.')
+    assert results_data.identical(reference_data), ('Output and reference files '
+                                                    'do not match.')
 
-    reference_data.close()
-    results_data.close()
     reference_data = None
     results_data = None
-
-
+    
+    
 def submit_and_download(harmony_client: Client, request: Request,
                         output_file_name: str):
     """ Submit a Harmony request via a `harmony-py` client. Wait for the
