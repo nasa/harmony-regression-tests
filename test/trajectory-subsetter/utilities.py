81827d72efedee7ad30702c62ffd002c9149c184
Matt Savoie <savoie@colorado.edu>
2024-08-06 10:56:58 -0600

    DAS-2214: Add the pre-commit file and update readme. (#86)

--- test/trajectory-subsetter/utilities.py
+++ test/trajectory-subsetter/utilities.py
@@ -3,6 +3,7 @@
     increase the readability of the regression test suite.
 
 """
+
 from os import listdir, remove, replace
 
 from harmony import Client, Request
@@ -10,28 +11,31 @@ from harmony.harmony import ProcessingFailedException
 from datatree import open_datatree
 
 
-def compare_results_to_reference_file(results_file_name: str,
-                                      reference_file_name: str) -> None:
-    """ Use `DataTree` functionality to compare data values, variables, 
-        coordinates, metadata, and all their corresponding attributes of
-        downloaded results to a reference file.
+def compare_results_to_reference_file(
+    results_file_name: str, reference_file_name: str
+) -> None:
+    """Use `DataTree` functionality to compare data values, variables,
+    coordinates, metadata, and all their corresponding attributes of
+    downloaded results to a reference file.
 
     """
-    reference_data =  open_datatree(reference_file_name)
+    reference_data = open_datatree(reference_file_name)
     results_data = open_datatree(results_file_name)
 
-    assert results_data.identical(reference_data), ('Output and reference files '
-                                                    'do not match.')
+    assert results_data.identical(reference_data), (
+        'Output and reference files ' 'do not match.'
+    )
 
     reference_data = None
     results_data = None
 
 
-def submit_and_download(harmony_client: Client, request: Request,
-                        output_file_name: str):
-    """ Submit a Harmony request via a `harmony-py` client. Wait for the
-        Harmony job to finish, then download the results to the specified file
-        path.
+def submit_and_download(
+    harmony_client: Client, request: Request, output_file_name: str
+):
+    """Submit a Harmony request via a `harmony-py` client. Wait for the
+    Harmony job to finish, then download the results to the specified file
+    path.
 
     """
     downloaded_filename = None
@@ -39,10 +43,10 @@ def submit_and_download(harmony_client: Client, request: Request,
     try:
         job_id = harmony_client.submit(request)
 
-        for filename in [file_future.result()
-                         for file_future
-                         in harmony_client.download_all(job_id,
-                                                        overwrite=True)]:
+        for filename in [
+            file_future.result()
+            for file_future in harmony_client.download_all(job_id, overwrite=True)
+        ]:
 
             print(f'Downloaded: {filename}')
             downloaded_filename = filename
@@ -57,8 +61,8 @@ def submit_and_download(harmony_client: Client, request: Request,
 
 
 def remove_results_files() -> None:
-    """ Remove all NetCDF-4 files downloaded during the Swath Projector
-        regression tests.
+    """Remove all NetCDF-4 files downloaded during the Swath Projector
+    regression tests.
 
     """
     directory_files = listdir()
@@ -69,10 +73,10 @@ def remove_results_files() -> None:
 
 
 def print_error(error_string: str) -> str:
-    """Print an error, with formatting for red text. """
+    """Print an error, with formatting for red text."""
     print(f'\033[91m{error_string}\033[0m')
 
 
 def print_success(success_string: str) -> str:
-    """ Print a success message, with formatting for green text. """
+    """Print a success message, with formatting for green text."""
     print(f'\033[92mSuccess: {success_string}\033[0m')
