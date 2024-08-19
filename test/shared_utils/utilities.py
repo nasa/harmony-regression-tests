df7dbd921791fd1ed2465bcb729e18bd5d456a90
Matt Savoie <matthew.savoie@colorado.edu>
2024-08-19 17:03:10 -0600

    DAS-2214: Adds First Temporal Subsetting Test
    ATL04 data doesn't work in DataTree because it's unalignable.
    I added a new routine compare_results_to_reference_file_new
    
    This needs to be replaced when trajectory subsetter switches over to use this,
    but I don't want to do that with an unreleased xarray version from main.

--- test/shared_utils/utilities.py
+++ test/shared_utils/utilities.py
@@ -5,13 +5,18 @@
 """
 
 from os import listdir, remove, replace
+from itertools import count
 
 from harmony import Client, Request
 from harmony.harmony import ProcessingFailedException
 
 try:
-    from xarray.backends.api import open_datatree
+    from xarray.backends.api import open_groups
+    from xarray.core.datatree import DataTree
+    from xarray import Dataset
 except Exception:
+    # only used by Trajectory Subsetter tests.
+    # TODO: remove and make Trajectory Subsetter use above
     from datatree import open_datatree
 
 
@@ -25,6 +30,35 @@ def compare_results_to_reference_file(
     """
     reference_data = open_datatree(reference_file_name)
     results_data = open_datatree(results_file_name)
+
+    assert results_data.identical(reference_data), (
+        'Output and reference files ' 'do not match.'
+    )
+
+    reference_data = None
+    results_data = None
+
+
+def compare_results_to_reference_file_new(
+    results_file_name: str,
+    reference_file_name: str,
+    coordinates_to_fix: list[str | None] = [],
+) -> None:
+    """Use `DataTree` functionality to compare data values, variables,
+    coordinates, metadata, and all their corresponding attributes of
+    downloaded results to a reference file.
+
+    """
+    reference_groups = open_groups(reference_file_name)
+    results_groups = open_groups(results_file_name)
+
+    # Fix unalignable coordinates
+    for coord in coordinates_to_fix:
+        reference_groups = unalign_groups(reference_groups, coord)
+        results_groups = unalign_groups(results_groups, coord)
+
+    reference_data = DataTree.from_dict(reference_groups)
+    results_data = DataTree.from_dict(results_groups)
 
     assert results_data.identical(reference_data), (
         'Output and reference files ' 'do not match.'
@@ -62,6 +96,46 @@ def submit_and_download(
     except ProcessingFailedException as exception:
         print_error('Harmony request failed to complete successfully.')
         raise exception
+
+
+def unalign_groups(
+    dict_of_datasets: dict[str, Dataset], coordinate: str
+) -> dict[str, Dataset]:
+    """Rename coordinates with different dimensions across datasets.
+
+    This function addresses the issue of datasets having coordinates with the
+    same name but different dimensions, which causes problems when creating a
+    DataTree. Specifically for handling data products like ATL04 ICESat2, where
+    common coordinates (e.g., "delta_time") have different lengths across
+    datasets.
+
+    The function renames the specified coordinate in each dataset where it appears,
+    assigning a unique identifier to each instance. This allows for the creation of
+    a DataTree from the modified dictionary of datasets.
+
+    Parameters:
+    -----------
+    dict_of_datasets : dict[str, Dataset]
+        A dictionary of xarray Datasets, typically obtained from xarray.open_groups().
+    coordinate : str
+        The name of the coordinate to be renamed across Datasets.
+
+    Returns:
+    --------
+    dict[str, Dataset]
+        A new dictionary of datasets with the specified coordinate
+        incrementally renamed when present.
+
+    """
+    counter = count(1)
+    return {
+        key: (
+            ds.rename({coordinate: f"{coordinate}_{next(counter)}"})
+            if coordinate in ds.coords
+            else ds
+        )
+        for key, ds in dict_of_datasets.items()
+    }
 
 
 def remove_results_files() -> None:
