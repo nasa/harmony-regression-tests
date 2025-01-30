6f13880e6aa1a689d134e34c8abbdbb4c0042865
Owen Littlejohns <owen.m.littlejohns@nasa.gov>
2025-01-30 17:55:24 -0500

    TRT-571 - Update regression tests for net2cog.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ versioning. Rather than a static releases, this repository contains of a number
 of regression tests that are each semi-independent.  This CHANGELOG file should be used
 to document pull requests to this repository.
 
+## 2025-01-30([#130](https://github.com/nasa/harmony-regression-tests/pull/130))
+
+- Updates net2cog tests to include single, multiple and all variable test cases.
+
 ## 2024-12-20([#126](https://github.com/nasa/harmony-regression-tests/pull/126))
 
 - Update configuration to run `opera-rtc-s1-browse` tests on HyBIG deployments.
--- test/net2cog/utility.py
+++ test/net2cog/utility.py
@@ -1,9 +1,15 @@
 """Simple utility functions used in the net2cog test notebook."""
 
+from os.path import basename
 from pathlib import Path
-import rasterio
+from tempfile import TemporaryDirectory
 import subprocess
+
+from harmony import Client
 from numpy.testing import assert_array_almost_equal
+from rasterio.crs import CRS
+from rasterio.transform import Affine
+import rasterio
 
 
 def print_error(error_string: str) -> str:
@@ -14,6 +20,81 @@ def print_error(error_string: str) -> str:
 def print_success(success_string: str) -> str:
     """Print a success message, with formatting for green text."""
     print(f'\033[92mSuccess: {success_string}\033[0m')
+
+
+def validate_smap_outputs(
+    harmony_client: Client, harmony_job_id: str, expected_file_count: int
+):
+    """Helper function to retrieve outputs from Harmony request and compare to reference
+    metadata and files.
+
+    Checks:
+
+    * The expected number of files are returned.
+    * The output files are valid Cloud Optimized GeoTIFFs.
+    * The data values and metadata match a reference file.
+
+    """
+    with TemporaryDirectory() as temp_dir:
+        downloaded_cog_outputs = [
+            file_future.result()
+            for file_future in harmony_client.download_all(
+                harmony_job_id, overwrite=True, directory=temp_dir
+            )
+        ]
+        assert len(downloaded_cog_outputs) == expected_file_count
+        print_success('Correct number of generated output files.')
+
+        for cog_file in downloaded_cog_outputs:
+            print(f'Assessing: {basename(cog_file)}')
+            validate_cog(cog_file)
+
+            expected_metadata = {
+                'driver': 'GTiff',
+                'dtype': get_expected_smap_dtype(cog_file),
+                'nodata': get_expected_smap_nodata(cog_file),
+                'width': 1440,
+                'height': 720,
+                'count': 1,
+                'crs': CRS.from_epsg(4326),
+                'transform': Affine(0.25, 0.0, 0.0, 0.0, 0.25, -90.0),
+            }
+            reference_file = Path(
+                './reference_data',
+                basename(cog_file),
+            )
+
+            assert_dataset_produced_correct_results(
+                cog_file, expected_metadata, reference_file
+            )
+
+
+def get_expected_smap_dtype(cog_file_name: str) -> str:
+    """Retrieve expected dtype for the COG metadata based on which
+    variable is being tested. The default value is 'float32', but
+    nobs and nobs_40km are 'int32' variables.
+
+    """
+    if 'nobs' in cog_file_name:
+        expected_dtype = 'int32'
+    else:
+        expected_dtype = 'float32'
+
+    return expected_dtype
+
+
+def get_expected_smap_nodata(cog_file_name: str) -> float:
+    """Retrieve expected nodata value for the COG metadata based on
+    which variable is being tested. The default value is -9999.0, but
+    nobs and nobs_40km have a nodata value of 0.0.
+
+    """
+    if 'nobs' in cog_file_name:
+        expected_nodata = 0.0
+    else:
+        expected_nodata = -9999.0
+
+    return expected_nodata
 
 
 def assert_dataset_produced_correct_results(
