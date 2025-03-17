d4301b3a380e1becc126450c40a8b911eb57ad50
owenlittlejohns <owen.m.littlejohns@nasa.gov>
2025-03-17 12:53:33 -0600

    TRT-571 - Update regression tests for net2cog. (#131)
    * TRT-571 - Update regression tests for net2cog.
    
    * TRT-571 - Directly compare output COG metadata to reference file.
    
    * TRT-571 - Add blank line to end of .gitattributes to appease pre-commit.
    
    ---------
    
    Co-authored-by: Matt Savoie <matthew.savoie@colorado.edu>

--- test/net2cog/utility.py
+++ test/net2cog/utility.py
@@ -1,9 +1,13 @@
 """Simple utility functions used in the net2cog test notebook."""
 
+from os.path import basename
 from pathlib import Path
-import rasterio
+from tempfile import TemporaryDirectory
 import subprocess
+
+from harmony import Client
 from numpy.testing import assert_array_almost_equal
+import rasterio
 
 
 def print_error(error_string: str) -> str:
@@ -16,17 +20,52 @@ def print_success(success_string: str) -> str:
     print(f'\033[92mSuccess: {success_string}\033[0m')
 
 
+def validate_smap_outputs(
+    harmony_client: Client, harmony_job_id: str, expected_file_count: int
+):
+    """Helper function to retrieve outputs from Harmony request and compare to reference
+    metadata and files.
+
+    Checks:
+
+    * The expected number of files are returned.
+    * The output files are valid Cloud Optimized GeoTIFFs.
+    * The data values and metadata match a reference file.
+
+    """
+    with TemporaryDirectory() as temp_dir:
+        downloaded_cog_outputs = [
+            file_future.result()
+            for file_future in harmony_client.download_all(
+                harmony_job_id, overwrite=True, directory=temp_dir
+            )
+        ]
+        assert len(downloaded_cog_outputs) == expected_file_count
+        print_success('Correct number of generated output files.')
+
+        for downloaded_cog_file in downloaded_cog_outputs:
+            print(f'Assessing: {basename(downloaded_cog_file)}')
+            validate_cog(downloaded_cog_file)
+
+            reference_file = Path(
+                './reference_data',
+                basename(downloaded_cog_file),
+            )
+
+            assert_dataset_produced_correct_results(downloaded_cog_file, reference_file)
+
+
 def assert_dataset_produced_correct_results(
-    generated_file: Path, expected_metadata: dict, reference_file: Path
+    generated_file: Path, reference_file: Path
 ) -> None:
     """Check that the generated data matches the expected data."""
     with rasterio.open(generated_file) as test_dataset:
-        assert (
-            test_dataset.meta == expected_metadata
-        ), f'output has incorrect metadata: {test_dataset.meta}'
-        print_success('Generated image has correct metadata.')
-
         with rasterio.open(reference_file) as reference_dataset:
+            assert (
+                test_dataset.meta == reference_dataset.meta
+            ), f'output has incorrect metadata: {test_dataset.meta}'
+            print_success('Generated image has correct metadata.')
+
             ref_image = reference_dataset.read()
             test_image = test_dataset.read()
             assert_array_almost_equal(ref_image, test_image)
