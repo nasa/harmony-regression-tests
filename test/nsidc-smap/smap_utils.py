6b07eb099875da641f4f4a95ae4ba6fb9203cb74
Matt Savoie <savoie@colorado.edu>
2025-08-26 16:08:51 -0600

    DAS-2397: Adds nsidc-smap regression test suite. (#204)

---
+++ test/nsidc-smap/smap_utils.py
@@ -0,0 +1,116 @@
+"""Collection of functions used for nsidc-smap regression tests."""
+
+from earthdata_hashdiff import (
+    create_geotiff_hash_file,
+    create_nc4_hash_file,
+    geotiff_matches_reference_hash_file,
+    nc4_matches_reference_hash_file,
+    h5_matches_reference_hash_file,
+)
+from pathlib import Path
+
+
+def file_for_variable(directory: Path, glob: str) -> Path:
+    """Return a Path to the matching single file.
+
+    Find a file in a directory by a glob string.
+
+    Raise error if mulitple files  match.
+    """
+    results = list(directory.glob(glob))
+    if len(results) != 1:
+        raise ValueError(f"Incorrect Number of files found ({len(results)}) for {glob}")
+
+    return results[0]
+
+
+def comparison_function_by_extension(ext: str) -> callable:
+    """Returns the correct function to call for the input file type's extension."""
+
+    compare_function_map = {
+        ".tif": geotiff_matches_reference_hash_file,
+        ".nc4": nc4_matches_reference_hash_file,
+        ".h5": h5_matches_reference_hash_file,
+    }
+    return compare_function_map[ext]
+
+
+def exclusions_by_extension(ext: str) -> dict:
+    """Returns the exclusion dictionary for comparison based on the extension."""
+
+    geotiff_exclusions = {}
+    nc4_and_h5_exclusions = {
+        "skipped_metadata_attributes": {
+            "build_dmrpp_metadata.invocation",
+            "build_dmrpp_metadata.configuration",
+            "Processing Parameters",
+        }
+    }
+
+    compare_function_map = {
+        ".tif": geotiff_exclusions,
+        ".nc4": nc4_and_h5_exclusions,
+        ".h5": nc4_and_h5_exclusions,
+    }
+    return compare_function_map[ext]
+
+
+def _generate_reference_files(test_config: dict, in_dir: Path, out_dir: Path):
+    """Process downloaded output into hashed files to be used as reference.
+
+    This routine is used to create the reference files for the the test.
+
+    It's NOT used to run the test.
+
+    Args:
+        test_config: Dictionary containing test configuration with request parameters
+        in_dir: Path to input directory containing downloaded data files
+        out_dir: Path to output directory where reference files will be saved
+
+    Example:
+        from smap_utils import generate_reference_files
+        from pathlib import Path
+
+        in_dir = Path('temporary-directory/')
+        out_dir = Path('new_reference_files/')
+
+        from non_prod_configuraton import non_production_configuration
+
+        generate_reference_files(non_production_configuration["multiple_output_tests"]["GeoTIFF_reformat"]["SPL2SMP"], in_dir, out_dir)
+
+    """
+    ## Generate reference files for the multifile request
+    for full_var in test_config["request_params"]["variables"]:
+        var = full_var.split("/")[-1]
+        in_fn = file_for_variable(in_dir, f"*Data_{var}_reformatted*")
+        out_fn = out_dir / f"{var}_reference.json"
+        print(f"generating geohash for {in_fn}")
+        create_geotiff_hash_file(str(in_fn), str(out_fn))
+
+    ## generate for the single file tests
+    input_files = [
+        Path("SPL2SMA_subset_bounding_box.tif"),
+        Path("SPL2SMA_subset_by_geojson.h5"),
+        Path("SPL2SMP_E_2_subset_by_variable.tif"),
+        Path("SPL2SMP_E_subset_by_variable.h5"),
+        Path("SPL3FTP_E_subset_bounding_box.nc4"),
+        Path("SPL3FTP_subset_by_shapefile.nc4"),
+        Path("SPL3SMP_subset_by_kml.nc4"),
+        Path("SPL4CMDL_reprojection_to_geographic.nc4"),
+    ]
+    for inbase in input_files:
+        out_fn = out_dir / f"{inbase.stem}_reference.json"
+        in_fn = in_dir / inbase
+        print(f"generating geohash/hash for {in_fn}")
+        if inbase.suffix == ".tif":
+            create_geotiff_hash_file(str(in_fn), str(out_fn))
+        else:
+            create_nc4_hash_file(
+                str(in_fn),
+                str(out_fn),
+                skipped_metadata_attributes={
+                    "build_dmrpp_metadata.invocation",
+                    "build_dmrpp_metadata.configuration",
+                    "Processing Parameters",
+                },
+            )
---
+++ test/nsidc-smap/test_configuraton.py
@@ -0,0 +1,214 @@
+from harmony import BBox, Collection
+import copy
+
+non_production_configuration = {
+    "single_output_tests": {
+        "subset_bounding_box": {
+            "SPL2SMA": {
+                "request_params": {
+                    "collection": Collection(id="C1242631429-NSIDC_CUAT"),
+                    "granule_id": "G1260377350-NSIDC_CUAT",
+                    "spatial": BBox(-8.2, 42.0, 12.7, 52.8),
+                    "variables": ["Radar_Data/sigma0_hh_mean"],
+                    "format": "image/tiff",
+                    "labels": ["smap-rtest-1", "smap-rtests"],
+                },
+                "test_params": {"ext": ".tif"},
+            },
+            "SPL3FTP_E": {
+                "request_params": {
+                    "collection": Collection(id="C1263071066-NSIDC_CUAT"),
+                    "granule_id": "G1263078049-NSIDC_CUAT",
+                    "spatial": BBox(-26, 63, -12, 67),
+                    "variables": [
+                        "Freeze_Thaw_Retrieval_Data_Polar/freeze_thaw",
+                        "Freeze_Thaw_Retrieval_Data_Polar/latitude",
+                        "Freeze_Thaw_Retrieval_Data_Polar/longitude",
+                    ],
+                    "labels": ["smap-rtest-15", "smap-rtests"],
+                },
+                "test_params": {"ext": ".nc4"},
+            },
+        },
+        "subset_by_geojson": {
+            "SPL2SMA": {
+                "request_params": {
+                    "collection": Collection(id="C1242631429-NSIDC_CUAT"),
+                    "granule_id": "G1260377350-NSIDC_CUAT",
+                    "shape": "ancillary/France_simple.geojson",
+                    "labels": ["smap-rtest-2", "smap-rtests"],
+                },
+                "test_params": {"ext": ".h5"},
+            },
+        },
+        "subset_by_shapefile": {
+            "SPL3FTP": {
+                "request_params": {
+                    "collection": Collection(id="C1263071065-NSIDC_CUAT"),
+                    "granule_id": "G1263076419-NSIDC_CUAT",
+                    "variables": [
+                        "Freeze_Thaw_Retrieval_Data_Polar/freeze_thaw",
+                        "Freeze_Thaw_Retrieval_Data_Polar/latitude",
+                        "Freeze_Thaw_Retrieval_Data_Polar/longitude",
+                        "Freeze_Thaw_Retrieval_Data_Polar/tbh_mean",
+                    ],
+                    "labels": ["smap-rtest-13", "smap-rtests"],
+                    "shape": "ancillary/Semey.zip",
+                },
+                "test_params": {"ext": ".nc4"},
+            },
+        },
+        "subset_by_variable": {
+            "SPL2SMP_E": {
+                "request_params": {
+                    "collection": Collection(id="C1263071064-NSIDC_CUAT"),
+                    "granule_id": "G1273163898-NSIDC_CUAT",
+                    "variables": [
+                        "Soil_Moisture_Retrieval_Data/latitude",
+                        "Soil_Moisture_Retrieval_Data/longitude",
+                        "Soil_Moisture_Retrieval_Data/soil_moisture",
+                        "Soil_Moisture_Retrieval_Data/soil_moisture_error",
+                        "Soil_Moisture_Retrieval_Data_Polar/latitude",
+                        "Soil_Moisture_Retrieval_Data_Polar/longitude",
+                        "Soil_Moisture_Retrieval_Data_Polar/tb_h_corrected",
+                        "Soil_Moisture_Retrieval_Data_Polar/tb_h_uncorrected",
+                    ],
+                    "labels": ["smap-rtest-10", "smap-rtests"],
+                },
+                "test_params": {"ext": ".h5"},
+            },
+            "SPL2SMP_E_2": {
+                "request_params": {
+                    "collection": Collection(id="C1242581633-NSIDC_CUAT"),
+                    "granule_id": "G1260703508-NSIDC_CUAT",
+                    "variables": ["Soil_Moisture_Retrieval_Data/soil_moisture"],
+                    "format": "image/tiff",
+                    "labels": ["smap-rtest-16", "smap-rtests"],
+                },
+                "test_params": {"ext": ".tif"},
+            },
+        },
+        "subset_by_kml": {
+            "SPL3SMP": {
+                "request_params": {
+                    "collection": Collection(id="C1263071067-NSIDC_CUAT"),
+                    "granule_id": "G1263078590-NSIDC_CUAT",
+                    "crs": "EPSG:4326",
+                    "shape": "ancillary/EasternUS.kml",
+                    "labels": ["smap-rtest-18", "smap-rtests"],
+                },
+                "test_params": {"ext": ".nc4"},
+            }
+        },
+        "reprojection_to_geographic": {
+            "SPL4CMDL": {
+                "request_params": {
+                    "collection": Collection(id="C1273193292-NSIDC_CUAT"),
+                    "granule_id": "G1273435798-NSIDC_CUAT",
+                    "crs": "EPSG:4326",
+                    "labels": ["smap-rtest-21", "smap-rtests"],
+                },
+                "test_params": {"ext": ".nc4"},
+            }
+        },
+    },
+    "multiple_output_tests": {
+        "GeoTIFF_reformat": {
+            # *** TEST 8 *** (generates a ton of GeoTIFFs. :grimmace: TODO [MHS, 08/15/2025] )
+            "SPL2SMP": {
+                "request_params": {
+                    "collection": Collection(id="C1263071063-NSIDC_CUAT"),
+                    "granule_id": "G1273158728-NSIDC_CUAT",
+                    "format": "image/tiff",
+                    "variables": [
+                        "Soil_Moisture_Retrieval_Data/EASE_column_index",
+                        "Soil_Moisture_Retrieval_Data/EASE_row_index",
+                        "Soil_Moisture_Retrieval_Data/albedo",
+                        "Soil_Moisture_Retrieval_Data/albedo_option3",
+                        "Soil_Moisture_Retrieval_Data/boresight_incidence",
+                        "Soil_Moisture_Retrieval_Data/bulk_density",
+                        "Soil_Moisture_Retrieval_Data/freeze_thaw_fraction",
+                        "Soil_Moisture_Retrieval_Data/grid_surface_status",
+                        "Soil_Moisture_Retrieval_Data/landcover_class_fraction",
+                        "Soil_Moisture_Retrieval_Data/landcover_class",
+                        "Soil_Moisture_Retrieval_Data/sand_fraction",
+                        "Soil_Moisture_Retrieval_Data/soil_moisture",
+                        "Soil_Moisture_Retrieval_Data/surface_flag",
+                        "Soil_Moisture_Retrieval_Data/tb_4_corrected",
+                        "Soil_Moisture_Retrieval_Data/tb_h_corrected",
+                        "Soil_Moisture_Retrieval_Data/tb_h_uncorrected",
+                        "Soil_Moisture_Retrieval_Data/tb_qual_flag_4",
+                        "Soil_Moisture_Retrieval_Data/tb_qual_flag_h",
+                        "Soil_Moisture_Retrieval_Data/tb_qual_flag_v",
+                        "Soil_Moisture_Retrieval_Data/vegetation_opacity_option3",
+                    ],
+                    "labels": ["smap-rtest-8", "smap-rtests"],
+                },
+                "test_params": {"ext": ".tif"},
+            },
+        },
+    },
+}
+
+
+## run the same tests with Different Production values for Collection and GranuleID
+production_overrides = {
+    ("single_output_tests", "subset_bounding_box", "SPL2SMA"): {
+        "collection": Collection(id="C2812935277-NSIDC_CPRD"),
+        "granule_id": "G2813012838-NSIDC_CPRD",
+    },
+    ("single_output_tests", "subset_bounding_box", "SPL3FTP_E"): {
+        "collection": Collection(id="C2938664439-NSIDC_CPRD"),
+        "granule_id": "G2940370330-NSIDC_CPRD",
+    },
+    ("single_output_tests", "subset_by_geojson", "SPL2SMA"): {
+        "collection": Collection(id="C2812935277-NSIDC_CPRD"),
+        "granule_id": "G2813012838-NSIDC_CPRD",
+    },
+    ("single_output_tests", "subset_by_shapefile", "SPL3FTP"): {
+        "collection": Collection(id="C2938664170-NSIDC_CPRD"),
+        "granule_id": "G2940362257-NSIDC_CPRD",
+    },
+    ("single_output_tests", "subset_by_variable", "SPL2SMP_E"): {
+        "collection": Collection(id="C2938663676-NSIDC_CPRD"),
+        "granule_id": "G3241947322-NSIDC_CPRD",
+    },
+    ("single_output_tests", "subset_by_variable", "SPL2SMP_E_2"): {
+        "collection": Collection(id="C2872766452-NSIDC_CPRD"),
+        "granule_id": "G2872934722-NSIDC_CPRD",
+    },
+    ("single_output_tests", "subset_by_kml", "SPL3SMP"): {
+        "collection": Collection(id="C2938664585-NSIDC_CPRD"),
+        "granule_id": "G2940373226-NSIDC_CPRD",
+    },
+    ("single_output_tests", "reprojection_to_geographic", "SPL4CMDL"): {
+        "collection": Collection(id="C3480440454-NSIDC_CPRD"),
+        "granule_id": "G3480457654-NSIDC_CPRD",
+    },
+    ("multiple_output_tests", "GeoTIFF_reformat", "SPL2SMP"): {
+        "collection": Collection(id="C3480440454-NSIDC_CPRD"),  #
+        "granule_id": "G3480457654-NSIDC_CPRD",
+    },
+}
+
+
+def _update_config_with_prod_values(in_config):
+    """Return a production configuration.
+
+    Make a copy of the input configuration and replace the collection and
+    granule ID with production values.
+
+    """
+    out_config = copy.deepcopy(in_config)
+
+    for (test_suite, test_type, short_name), p_config in production_overrides.items():
+        out_config[test_suite][test_type][short_name]["request_params"][
+            "collection"
+        ] = p_config["collection"]
+        out_config[test_suite][test_type][short_name]["request_params"][
+            "granule_id"
+        ] = p_config["granule_id"]
+    return out_config
+
+
+production_configuration = _update_config_with_prod_values(non_production_configuration)
