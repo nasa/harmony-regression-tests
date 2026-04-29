d6098adb352ec4de65162bacd4a9f8ac43d9ea1e
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2026-04-29 09:14:06 -0800

    style: re-format assertion failure message f-string for linter

--- test/net2cog/utility.py
+++ test/net2cog/utility.py
@@ -174,7 +174,8 @@ def validate_bounding_box_and_plot_cog_file(
     with rasterio.open(cog_file) as src:
         raster_data = src.read(1)  # Read the first band
 
-        assert src.bounds in expected_results['expected_bounding_box'], f'Bounds did not match: Expected {expected_results["expected_bounding_box"]}, got {src.bounds}'
+        expected_bboxs = expected_results["expected_bounding_box"]
+        assert src.bounds in expected_bboxs, f'Bounds did not match: Expected {expected_bboxs}, got {src.bounds}'
         print_success(f"Correct Bounding Box: {src.bounds}")
 
         extent = (
